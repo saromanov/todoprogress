51f4fc678fcb32ce055ab638bdd5fe118a83d344
Sergey Romanov <xxsmotur@gmail.com>
2014-05-03 05:08:00 +0600

    Split load and prepare data

--- learning.py
+++ learning.py
@@ -2,20 +2,33 @@ from sklearn import linear_model
 from sklearn.neighbors.nearest_centroid import NearestCentroid
 import numpy as np
 import json
+import os
 
-#Load data from json
-def loadData(path, fields, params):
+def loadData(path):
+	if not os.path.isfile(path):
+		raise IOError("File not found")
+	return json.loads(open(path).read())
+
+def prepareData(datajson, fields, param):
+	'''
+	datastore - data in json format
+	fields - fields will be use in learning
+	param - target value for predict
+	'''
+
 	X = []
 	y = []
-	value = json.loads(open(path).read())
-	for v in value:
-		data = value[v]
+	for v in datajson:
+		data = datajson[v]
 		temp = []
 		for f in fields:
 			temp.append(data[f])
 		X.append(temp)
 		y.append(data[params])
 	return X,y
+
+
+#Split data on test set and train set
 
 def Regression(X, y, pred_value):
 	regr = linear_model.BayesianRidge()\
@@ -33,5 +46,8 @@ def task_clustering(X, y, pred_value):
 #Predict on succes or failure of task
 def predict_success(X, y, pred_value, threshold):
 	True if Regression(X, y, pred_value) >= threshold else False
+
+
+prepareData(loadData("../task_data.json"), ["time", "value"], "result")
 
 
