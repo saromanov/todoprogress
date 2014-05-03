0834fb70934c7a633ab0becd13d8f1b8212ceb70
Sergey Romanov <xxsmotur@gmail.com>
2014-05-04 01:09:46 +0600

    Set constructor for find similar tasks

--- learning.py
+++ learning.py
@@ -1,5 +1,7 @@
 from sklearn import linear_model
 from sklearn.neighbors.nearest_centroid import NearestCentroid
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.feature_selection import SelectKBest
 import numpy as np
 import json
 import os
@@ -9,7 +11,7 @@ def loadData(path):
 		raise IOError("File not found")
 	return json.loads(open(path).read())
 
-def prepareData(datajson, fields, param):
+def prepareData(datajson, fields, param=None):
 	'''
 	datastore - data in json format
 	fields - fields will be use in learning
@@ -24,7 +26,8 @@ def prepareData(datajson, fields, param):
 		for f in fields:
 			temp.append(data[f])
 		X.append(temp)
-		y.append(data[param])
+		if param != None:
+			y.append(data[param])
 	return X,y
 
 
@@ -46,6 +49,30 @@ def task_clustering(X, y, pred_value):
 #Predict on succes or failure of task
 def predict_success(X, y, pred_value, threshold):
 	True if Regression(X, y, pred_value) >= threshold else False
+
+
+#Find similar tasks
+def find_similar_tasks(X, y):
+	'''
+		Get list of most probable tasks from task name and tags
+		names=['name_1', name_n']
+
+		X,y = prepareData(loadData("../task_data.json"), ['task'], 'complete')
+	'''
+	clear = [i[0] for i in X]
+	vect = CountVectorizer()
+	vmatrix = vect.fit_transform(clear)
+	#print(vmatrix)
+	tfifd = TfidfVectorizer(stop_words="english")
+	X_train = tfifd.fit_transform(clear)
+	ch = SelectKBest()
+	result = ch.fit_transform(X_train, y)
+	return ch.fit_transform(X_train, y)
+
+
+X,y = prepareData(loadData("../task_data.json"), ['task'], 'complete')
+find_similar_tasks(X, y)
+
 
 
 
