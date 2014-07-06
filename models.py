213f2b834ceda659547ef4470d3f815e07141582
Sergey Romanov <xxsmotur@gmail.com>
2014-07-07 01:30:14 +0600

    Append models.py for forms

---
+++ models.py
@@ -0,0 +1,63 @@
+from wtforms import TextField, HiddenField, ValidationError,\
+ SubmitField, IntegerField, FormField, TextAreaField, SelectField, validators, DateTimeField,\
+ PasswordField
+from wtforms.validators import Required, DataRequired, InputRequired
+from peewee import Model
+from flask_bootstrap import Bootstrap
+from flask_wtf import Form, RecaptchaField
+
+from util import *
+
+TypeOfTaskField = SelectField("Type of task", choices=(("study", "study"), \
+		("read", "read"), \
+		("fun", "fun"),
+		("work", "work"),
+		("programming", "programming")))
+
+#Move ths stuff to models file
+class TodoForm(Form):
+	tf = TextField("Task")
+	descr = TextField("Desription")
+	type_of_task = TypeOfTaskField
+	priority_field = SelectField("Priority of task", choices = (("middle", "Middle"),\
+		("low", "Low"), ("high", "High")))
+	tags = TextField("Tags")
+	starttime = DateTimeField(default= roundStartTime(datetime.datetime.now()))
+
+	#Replace for vetter solution
+	deadline = IntegerField("Deadline")
+	submit = SubmitField("Add")
+
+
+
+class BaseModel(Model):
+	join_field = DateTimeField()
+class BeforeTaskForm(Form):
+	'''
+		Adding information after completion of task
+	'''
+
+	#Replace for better solution
+	task_id = TextField("task")
+	mark = IntegerField("Your mark")
+	iscomplete = SelectField("This task was complete before deadline?", choices=(\
+		("no", "No"),
+		("yes", "Yes")))
+
+
+
+class SearchForm(Form):pass
+
+
+class PlanningForm(Form):
+	tf = TextField("Task")
+	ttype = TypeOfTaskField
+	deadline = TextField("deadline", description="A")
+	starttime = DateTimeField(default=defaultTime)
+	endtime = DateTimeField(default=defaultTime)
+
+
+
+class Login(Form):
+	nick = TextField("nickname")
+	password = PasswordField("password")
--- run.py
+++ run.py
@@ -1,19 +1,12 @@
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 import sqlite3
 from contextlib import closing
-from flask_bootstrap import Bootstrap
-from flask_wtf import Form, RecaptchaField
-from wtforms import TextField, HiddenField, ValidationError,\
- SubmitField, IntegerField, FormField, TextAreaField, SelectField, validators, DateTimeField,\
- PasswordField
-from wtforms.validators import Required, DataRequired, InputRequired
-from peewee import Model
-
 from pymongo import MongoClient
 
 from learning import predict_success, gaussianPredict, getData, findOptimalTime, \
 planning_task_list
 from util import *
+from models import *
 from db import DB
 
 import random
@@ -21,12 +14,6 @@ import os
 import datetime
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
-TypeOfTaskField = SelectField("Type of task", choices=(("study", "study"), \
-		("read", "read"), \
-		("fun", "fun"),
-		("work", "work"),
-		("programming", "programming")))
 
 DEBUG=True
 SECRET_KEY = 'secret'
@@ -40,54 +27,6 @@ tasks = db.tasks
 trash = db.trash
 dbdata = DB(tasks, trash)
 
-
-#Move ths stuff to models file
-class TodoForm(Form):
-	tf = TextField("Task")
-	descr = TextField("Desription")
-	type_of_task = TypeOfTaskField
-	priority_field = SelectField("Priority of task", choices = (("middle", "Middle"),\
-		("low", "Low"), ("high", "High")))
-	tags = TextField("Tags")
-	starttime = DateTimeField(default= roundStartTime(datetime.datetime.now()))
-
-	#Replace for vetter solution
-	deadline = IntegerField("Deadline")
-	submit = SubmitField("Add")
-
-
-
-class BaseModel(Model):
-	join_field = DateTimeField()
-class BeforeTaskForm(Form):
-	'''
-		Adding information after completion of task
-	'''
-
-	#Replace for better solution
-	task_id = TextField("task")
-	mark = IntegerField("Your mark")
-	iscomplete = SelectField("This task was complete before deadline?", choices=(\
-		("no", "No"),
-		("yes", "Yes")))
-
-
-
-class SearchForm(Form):pass
-
-
-class PlanningForm(Form):
-	tf = TextField("Task")
-	ttype = TypeOfTaskField
-	deadline = TextField("deadline", description="A")
-	starttime = DateTimeField(default=defaultTime)
-	endtime = DateTimeField(default=defaultTime)
-
-
-
-class Login(Form):
-	nick = TextField("nickname")
-	password = PasswordField("password")
 
 @app.route("/", methods=("GET", "POST"))
 def main():
